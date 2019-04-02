c917a72dbf09599fa668f40613d71d5b29b825ca
Stefan Schmidt <stefanigit@knallakoff.de>
2019-04-02 17:38:34 +0200

    v2 works better but gives segmentation fauls and looks like sometimes the GPIOs are crashing?  not sur why... maybe it is the raspian, I used dietpi before

--- gamepad.py
+++ gamepad.py
@@ -18,8 +18,11 @@ while True:
 #prints out device info at start
 print(gamepad)
 
-def scaleSpeed ( x, in_min=0,  in_max=65535,  out_min=-99,  out_max=99):
+def scaleSpeed ( x, in_min=0,  in_max=65535,  out_min=-100,  out_max=100):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+
 
 xSpeed = 0
 ySpeed = 0
@@ -28,118 +31,77 @@ rightSpeed = 0
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
 
-        #filters by event type
-            if event.type == ecodes.EV_KEY:
-                if event.code == 304 and event.value == 1:
-                    bOpressed = True
+    #filters by event type
+    if event.type == ecodes.EV_KEY:
+        if event.code == 304 and event.value == 1:
+            bOpressed = True
+            robomove.enable_left_motor(0)
+            robomove.enable_right_motor(0)
+            leftSpeed  = 0
+            rightSpeed = 0
+            xSpeed  = 0
+            ySpeed = 0            
+        else:
+            bOpressed = False
+        if event.code == 312 and event.value == 1:
+            bDPadUpPressed = True
+            
+        else:
+            bDPadUpPressed = False
+        if bDPadUpPressed and bOpressed:
+            starttime = time.epoch()
+        
+
+        #print(event.code, event.sec, event.timestamp)
+        print(event)
+    elif event.type == ecodes.EV_ABS:
+                #print(event)
+                #print ('button/axis: %s : %s' %(event.code,event.value))
+                # code 1 y axis ( forwad/ backward)^
+                '''
+                if event.code == 2 and event.value >= 26000:
+                    leftSpeed = scaleSpeed(event.value, in_min=26000,  in_max=65535, out_min=0,  out_max=100) *-1
+                    print ('button/axis: %s : %s' %(event.code,event.value))
+                if event.code == 5:
+                    rightSpeed = scaleSpeed(event.value, out_min=0,  out_max=100) *-1
+                    print ('button/axis: %s : %s' %(event.code,event.value))
+
+                if 304 in gamepad.active_keys():
+                    robomove.enable_left_motor(-leftSpeed)
+                    robomove.enable_right_motor(-rightSpeed)
                 else:
-                    bOpressed = False
-                if event.code == 312 and event.value == 1:
-                    bDPadUpPressed = True
-                    
-                else:
-                    bDPadUpPressed = False
-                if bDPadUpPressed and bOpressed:
-                    starttime = time.epoch()
+                    robomove.enable_left_motor(leftSpeed)
+                    robomove.enable_right_motor(rightSpeed)
+
                 
-
-                #print(event.code, event.sec, event.timestamp)
-                print(event)
-            elif event.type == ecodes.EV_ABS:
-                        #print(event)
-                        #print ('button/axis: %s : %s' %(event.code,event.value))
-                        # code 1 y axis ( forwad/ backward)^
-                        '''
-                        if event.code == 2 and event.value >= 26000:
-                            leftSpeed = scaleSpeed(event.value, in_min=26000,  in_max=65535, out_min=0,  out_max=100) *-1
-                            print ('button/axis: %s : %s' %(event.code,event.value))
-                        if event.code == 5:
-                            rightSpeed = scaleSpeed(event.value, out_min=0,  out_max=100) *-1
-                            print ('button/axis: %s : %s' %(event.code,event.value))
-
-                        if 304 in gamepad.active_keys():
-                            robomove.enable_left_motor(-leftSpeed)
-                            robomove.enable_right_motor(-rightSpeed)
-                        else:
-                            robomove.enable_left_motor(leftSpeed)
-                            robomove.enable_right_motor(rightSpeed)
-
-                        
-                        
-                        continue
-                        '''
-                        if event.code == 1:
-                            ySpeed = scaleSpeed(event.value) *-1
-                            '''
-                            if ySpeed < 0:
-                                print ('Forward: %s'% ySpeed)
-                            elif ySpeed > 0:
-                                print ('Backward: %s'% ySpeed)
-                            else:
-                                print ('For-/Backward: %s'% ySpeed)
-                            '''
-                        #else:
-                        #    ySpeed = 0
-                        if event.code == 0:
-                            xSpeed = scaleSpeed(event.value) *-1
-                            '''if xSpeed < 0:
-                                print ('Left: %s'% xSpeed)
-                            elif xSpeed > 0:
-                                print ('Right: %s'% xSpeed)
-                            else:
-                                print ('Left / Right: %s'% xSpeed)
-                            '''
-                        #else:
-                        #    xSpeed = 0
-                        
-#                        leftSpeed = scaleSpeed(ySpeed + xSpeed, -200, 200, -100, 100)
-#                        rightSpeed = scaleSpeed(ySpeed - xSpeed, -200, 200, -100, 100)
-                        print ('ySpeed: %s / xSpeed: %s'% (ySpeed, xSpeed))
-                        #if ySpeed > 0  and xSpeed == 0:
-                        #    leftSpeed =  ySpeed - xSpeed
-                        #    rightSpeed = ySpeed + xSpeed
-                        #    print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
-                        #elif ySpeed < 0  and xSpeed == 0:
-                        #    leftSpeed =  ySpeed + xSpeed
-                        #    rightSpeed = ySpeed - xSpeed
-                        #    print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
-                        if ySpeed >= 0:#xSpeed:
-                        #if True:
-                            leftSpeed =  ySpeed
-                            rightSpeed = ySpeed
-                            if xSpeed >= 0:
-                                leftSpeed -= xSpeed
-                            else:
-                                rightSpeed -= abs(xSpeed)
-
-                        else:
-                            leftSpeed =  ySpeed
-                            rightSpeed = ySpeed
-                            if xSpeed >= 0:
-                                leftSpeed += xSpeed
-                            else:
-                                rightSpeed += abs(xSpeed)                            
-                        
-                        if ySpeed ==0 and xSpeed != 0:
-                            leftSpeed =  xSpeed * -1
-                            rightSpeed = xSpeed
-                        #if ySpeed ==0 and xSpeed < 0:
-                        #    leftSpeed =  xSpeed 
-                        #    rightSpeed = xSpeed * -1
-                        #    print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
+                
+                continue
+                '''
+                if event.code == 1:
+                    ySpeed = event.value
+                    ySpeed = scaleSpeed(ySpeed) *-1
+                if event.code == 0:
+                    xSpeed = event.value
+                    xSpeed = scaleSpeed(xSpeed) #*-1
+                
+                RpL = (100-abs(xSpeed)) * (ySpeed/100) + ySpeed
+                RmL = (100-abs(ySpeed)) * (xSpeed/100) + xSpeed
+                leftSpeed =  (RpL + RmL)/2
+                rightSpeed = (RpL - RmL)/2
 
 
+                #print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
+                leftSpeed = scaleSpeed(leftSpeed, in_min=-100, in_max=100, out_min=-50, out_max=50)
+                rightSpeed = scaleSpeed(rightSpeed, in_min=-100, in_max=100, out_min=-50, out_max=50)
+                #print ('\nySpeed: {0}\t-\txSpeed: {1}\nRpL: {2}\t-\tRmL: {3}\nleftSpeed: {4}\t-\trightSpeed: {5}\n'
+                #            .format(ySpeed, xSpeed, RpL, RmL, leftSpeed, rightSpeed))
 
-                        #if ySpeed > 0  and xSpeed > 0:
-                        #    leftSpeed =  ySpeed - xSpeed
-                        #    rightSpeed = ySpeed
-                        #    print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
-                        #elif ySpeed == 0  and xSpeed < 0:
-                        #    leftSpeed =  ySpeed + xSpeed
-                        #    rightSpeed = ySpeed - xSpeed
-                        #    print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
-                        print ('Leftmotor: %s / Rightmotor: %s'% (leftSpeed, rightSpeed))
-                        robomove.enable_left_motor(leftSpeed)
-                        robomove.enable_right_motor(rightSpeed)
-                            
-
+                robomove.enable_left_motor(leftSpeed)
+                robomove.enable_right_motor(rightSpeed)
+                    
+                #robomove.enable_left_motor(0)
+                #robomove.enable_right_motor(0)
+                leftSpeed  = 0
+                rightSpeed = 0
+                xSpeed  = 0
+                ySpeed = 0
